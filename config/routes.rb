3800ebcc7e812ede8771e890e4ee3fa506ffea30
raphael hidalgo <raphael.hidalgo@ieseg.fr>
2022-08-19 14:41:11 +0200

    problem solved

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get "", to: "lists#index"
   # Defines the root path route ("/")
   # root "articles#index"
   resources :lists, only: %w[index show new create] do
