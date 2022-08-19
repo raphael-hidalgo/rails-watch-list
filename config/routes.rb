4d42e779e4af0028d068720703761728e91a68d7
raphael hidalgo <raphael.hidalgo@ieseg.fr>
2022-08-19 12:16:47 +0200

    rspec all good

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :lists, only: %w[index show new create] do
+    resources :bookmarks, only: %w[new create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
+
+# GET "lists"
+# A user can see the details of a given list and its name
+# GET "lists/42"
+# A user can create a new list
+# GET "lists/new"
+# POST "lists"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts "Cleaning Movies"
+Bookmark.destroy_all
+List.destroy_all
+Movie.destroy_all
+
+puts "Fake it until you make it"
+list = List.new(name: "all movies")
+list.save
+movie1 = Movie.create!(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
+bookmark1 = Bookmark.create!(comment:"abcdef", list_id: list.id, movie_id: movie1.id)
+movie2 = Movie.create!(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
+bookmark2 = Bookmark.create!(comment:"abcdef", list_id: list.id, movie_id: movie2.id)
+movie3 = Movie.create!(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
+bookmark3 = Bookmark.create!(comment:"abcdef", list_id: list.id, movie_id: movie3.id)
+movie4 = Movie.create!(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+bookmark4 = Bookmark.create!(comment:"abcdef", list_id: list.id, movie_id: movie4.id)
+Movie.create!(title: "TEST", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
+puts "You did it"
